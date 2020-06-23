e1138c1d3098fb98ad5085e07ec523c88d593dfe
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 17:45:19 +0300

    Добавьте файлы проекта.

---
+++ Euler 7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Euler_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int k = 2;
+            while(true)
+            {
+                if (IsSimple(k))
+                {
+                    if (number == 10001)
+                    {
+                        Console.WriteLine($"The number {k} is a 10001 prime number");
+                        return;
+                    }
+                    number++;
+                }
+                ++k;
+                
+            }
+        }
+
+        static bool IsSimple(int number)
+        {
+            for(int i = 2; i < number/2; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
